9d9422b59e0db3e54eb53b56153d811852666d12
r21nomi <ryotakosu@gmail.com>
2020-03-30 00:57:21 +0900

    Add contact form

---
+++ components/ContactForm.vue
@@ -0,0 +1,36 @@
+<template>
+    <form
+            class="contactForm"
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+    >
+        <input type="hidden" name="form-name" value="contact" />
+        <label>
+            <input type="text" v-model="subject" name="subject" />
+        </label>
+        <label>
+            <textarea v-model="message" name="message" />
+        </label>
+        <button>Submit</button>
+    </form>
+</template>
+<script>
+    export default {
+        name: "ContactForm",
+        data () {
+            return {
+                subject: "",
+                message: ""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .contactForm {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
